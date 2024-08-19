1bc2787dae8f71f101013ea715bad7f844abce3a
Sanjeeb Padhi <sanjeebpadhi96@gmail.com>
2024-08-19 17:22:44 +0530

    Counter project completed successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Initial state
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // Increment handler
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  // Decrement handler
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <div>
+          <button onClick={this.handleIncrement}>Increment</button>
+          <button onClick={this.handleDecrement}>Decrement</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
